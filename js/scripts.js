dd7d37d2f9423a4d02f5903fd08b30103ec65645
Yukino Kondo <yukino.kondo@gmail.com>
2018-08-14 03:30:46 +0200

    update again

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,8 @@
 $('#fullpage').fullpage({
   sectionSelector: '.vertical-scrolling',
-  controlArrows: false
+  controlArrows: false,
+  scrollingSpeed: 700,
+  lockAnchors: false,
+  anchors: ['firstPage', 'secondPage', 'thirdPage', 'fourthPage', 'fifthPage']
   // more options here
 });
